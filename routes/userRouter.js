d0dbcb9cccb6648aed16fd05f11e1b19ac92e771
2785448g <2785448g@student.gla.ac.uk>
2024-01-13 16:27:55 +0800

    Updated bug where file cant be uploaded

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -6,7 +6,7 @@ import upload from "../config/fileUpload.js";
 
 const userRouter = express.Router();
 
-userRouter.post('/register', isLoggedIn , isAdmin, userRegistrationController);
+userRouter.post('/register', isLoggedIn , isAdmin, upload.single('file'),  userRegistrationController);
 userRouter.post('/login', userLoginController);
 userRouter.get('/allUsers', isLoggedIn , isAdmin, getAllUserController);
 userRouter.put('/:id/updateUsers', isLoggedIn , isAdmin, upload.single('file'), updateUserController);
