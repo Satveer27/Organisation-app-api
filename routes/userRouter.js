883acfcce3bdb65c4171d30fede1e8ad0d63ffb0
2785448g <2785448g@student.gla.ac.uk>
2024-02-05 23:27:17 +0000

    Updated a very small bug

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -8,10 +8,11 @@ const userRouter = express.Router();
 
 userRouter.post('/register', isLoggedIn , isAdmin, upload.single('file'),  userRegistrationController);
 userRouter.post('/login', userLoginController);
+userRouter.get('/zone', getUserByZone);
 userRouter.get('/allUsers', isLoggedIn , isAdmin, getAllUserController);
 userRouter.put('/:id/updateUsers', isLoggedIn , isAdmin, upload.single('file'), updateUserController);
 userRouter.delete('/:id/delete', isLoggedIn , isAdmin, deleteUserController);
 userRouter.get('/:id', isLoggedIn , isAdmin, getSingleUser);
-userRouter.get('/zone',getUserByZone);
+
 
 export default userRouter;
